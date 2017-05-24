791d7d0132df6a331179bc74384b42fca0249e46
dingnate <dingnate@qq.com>
2017-05-24 15:44:05 +0800

    修改菜单名称Generate Java Data->Generate Java Json

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ jar包从附件或者http://git.oschina.net/dingnate/code-parser/releases 获取
 将jar包放到eclipse根目录的plugins目录中，重启eclipse。
 
 ### 使用
-在工程中选中java文件，可以看到右键菜单Generate Java Data，点击菜单执行成功后默认会在工程根目录下生成"code.parser.result.json"文件。
+在工程中选中java文件，可以看到右键菜单Generate Java Json，点击菜单执行成功后默认会在工程根目录下生成"code.parser.result.json"文件。
 该文件中保存了java文件的接口信息。
 
 在工程中选中java文件，可以看到右键菜单Generate Java Xls，点击菜单执行成功后默认会在工程根目录下生成"code.parser.result.xls"文件。
---
+++ src/code/parser/popup/actions/GenerateJavaJson.java
@@ -0,0 +1,103 @@
+package code.parser.popup.actions;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+import code.parser.dialog.FileSaveDialog;
+import code.parser.util.ASTUtil;
+import code.parser.util.UIUtil;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * GenerateJavaJson
+ * 
+ * @author dinghn
+ *
+ */
+public class GenerateJavaJson implements IObjectActionDelegate {
+
+	private Shell shell;
+	private Object[] selects;
+
+	/**
+	 * Constructor for GenerateJavaJson.
+	 */
+	public GenerateJavaJson() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		if (selects == null) {
+			return;
+		}
+		try {
+			if (!(selects[0] instanceof IFile))
+				return;
+			IProject project = ((IFile) selects[0]).getProject();
+			String path = new FileSaveDialog(shell,
+					"please select the file to save",
+					"code.parser.result.json", project.getLocation()
+							.toOSString(), new String[] { "*.json" },
+					new String[] { "JSON文件 (*.json)" }, true).open();
+			if (StringUtils.isEmpty(path))
+				return;
+
+			JSONArray array = new JSONArray();
+			for (Object object : selects) {
+				if (!(object instanceof IFile)) {
+					continue;
+				}
+				IFile ifile = (IFile) object;
+				JSONObject parse = ASTUtil.parse(FileUtils
+						.readFileToString(ifile.getLocation().toFile()));
+				array.add(parse);
+			}
+
+			File outFile = new File(path);
+			outFile.createNewFile();
+			FileUtils.writeStringToFile(outFile, array.toJSONString());
+			UIUtil.refresh(project, outFile);
+			MessageDialog.openInformation(shell, "Parser",
+					"Generate java json was executed.");
+		} catch (Exception e) {
+			MessageDialog.openError(shell, "Parser",
+					"Generate java json write Failed.\n" + e.toString());
+		}
+
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (!(selection instanceof IStructuredSelection))
+			return;
+		IStructuredSelection ss = (IStructuredSelection) selection;
+		selects = ss.toArray();
+	}
+
+}
